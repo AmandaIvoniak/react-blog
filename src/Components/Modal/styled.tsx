dbf5d985a22170c38f8c32faf4fbe32123c5f01f
a.ivoniak <a.ivoniak@vhsys.com>
2023-02-13 11:00:31 -0300

    Border modal

--- src/Components/Modal/styled.tsx
+++ src/Components/Modal/styled.tsx
@@ -13,7 +13,6 @@ export const Modal = styled.div<ModalProps>`
   width: 100%;
   height: 100%;
   overflow: auto;
-  background-color: rgb(0,0,0);
   background-color: rgba(0,0,0,0.4);
   .contentCard {
     background-color: #fefefe;
@@ -21,5 +20,6 @@ export const Modal = styled.div<ModalProps>`
     padding: 20px;
     border: 1px solid #888;
     width: 80%;
+    border-radius: 8px;
   }
 `;
